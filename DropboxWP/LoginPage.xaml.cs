b5de9c9a021b3d985d5409506547b54a0962f7a5
Timo Strömmer <timo.strommer@symbio.com>
2011-11-13 14:27:51 +0100

    Merged token functions

--- DropboxWP/LoginPage.xaml.cs
+++ DropboxWP/LoginPage.xaml.cs
@@ -37,72 +37,7 @@ namespace DropboxWP
 
         private void LoadRequestToken()
         {
-            string baseUrl = "https://api.dropbox.com/1/oauth/request_token";
-            string consumerKey = "<enter-key>";
-            string consumerSecret = "<enter-secret>";
-            long timestamp = GetEpochTime();
-            int nonce = new Random().Next(10000000);
-
-            string signedParams = "oauth_consumer_key=" + consumerKey
-                + "&oauth_nonce=" + nonce
-                + "&oauth_signature_method=HMAC-SHA1"
-                + "&oauth_timestamp=" + timestamp
-                + "&oauth_version=1.0";
-
-            byte[] signKey = Encoding.UTF8.GetBytes(UpperCaseUrlEncode(consumerSecret) + "&");
-            byte[] urlToSign = Encoding.UTF8.GetBytes("GET&" + UpperCaseUrlEncode(baseUrl) + "&" + UpperCaseUrlEncode(signedParams));
-            byte[] hash = new HMACSHA1(signKey).ComputeHash(urlToSign);
-            string signature = UpperCaseUrlEncode(Convert.ToBase64String(hash, 0, hash.Length));
-
-            string authorizationHeader = "OAuth oauth_version=\"1.0\""
-                + ", oauth_consumer_key=\"" + consumerKey + "\""
-                + ", oauth_nonce=\"" + nonce + "\""
-                + ", oauth_signature_method=\"HMAC-SHA1\""
-                + ", oauth_timestamp=\"" + timestamp + "\""
-                + ", oauth_signature=\"" + signature + "\"";
-
-            Debug.WriteLine("INPUT: " + Encoding.UTF8.GetString(urlToSign, 0, urlToSign.Length));
-            Debug.WriteLine("SIGNATURE: " + signature);
-
-            HttpWebRequest request = WebRequest.CreateHttp(baseUrl);
-            request.Headers[HttpRequestHeader.Authorization] = authorizationHeader;
-            request.AllowReadStreamBuffering = true;
-            request.BeginGetResponse(r =>
-            {
-                HttpWebRequest httpRequest = (HttpWebRequest)r.AsyncState;
-                try
-                {
-//                    Debug.WriteLine(request.Headers.ToString());
-                    HttpWebResponse httpResponse = httpRequest.EndGetResponse(r) as HttpWebResponse;
-                    StreamReader reader = new StreamReader(httpResponse.GetResponseStream());
-                    string responseText = reader.ReadToEnd();
-                    string[] list = responseText.Split('&');
-                    string token = "";
-                    string secret = "";
-                    foreach (string s in list)
-                    {
-                        string[] nameval = s.Split('=');
-                        if (nameval.Length == 2)
-                        {
-                            if (nameval[0] == "oauth_token") { token = nameval[1]; }
-                            else if (nameval[0] == "oauth_token_secret") { secret = nameval[1]; }
-                        }
-                    }
-                    if (token.Length > 0 && secret.Length > 0)
-                    {
-                        // Step 1 complete, got unauthorized token and secret
-                        //  -> Step 2, launch web browser
-                        requestToken = token;
-                        requestTokenSecret = secret;
-                        Debug.WriteLine("Unauthorized: Token: " + requestToken + ", Secret: " + requestTokenSecret);
-                        Dispatcher.BeginInvoke(new Action(() => webBrowser1.Source = new Uri("https://www.dropbox.com/1/oauth/authorize?oauth_token=" + token + "&oauth_callback=http://localhost")));
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Debug.WriteLine(ex.Message);
-                }
-            }, request);
+            LoadToken("https://api.dropbox.com/1/oauth/request_token", true);
         }
 
         private void webBrowser1_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
@@ -118,7 +53,11 @@ namespace DropboxWP
 
         private void LoadAccessToken()
         {
-            string baseUrl = "https://api.dropbox.com/1/oauth/access_token";
+            LoadToken("https://api.dropbox.com/1/oauth/access_token", false);
+        }
+
+        private void LoadToken(string baseUrl, bool isRequestToken)
+        {
             string consumerKey = "<enter-key>";
             string consumerSecret = "<enter-secret>";
             long timestamp = GetEpochTime();
@@ -128,10 +67,10 @@ namespace DropboxWP
                 + "&oauth_nonce=" + nonce
                 + "&oauth_signature_method=HMAC-SHA1"
                 + "&oauth_timestamp=" + timestamp
-                + "&oauth_token=" + requestToken
+                + (isRequestToken ? "" : "&oauth_token=" + requestToken)
                 + "&oauth_version=1.0";
 
-            byte[] signKey = Encoding.UTF8.GetBytes(UpperCaseUrlEncode(consumerSecret) + "&" + UpperCaseUrlEncode(requestTokenSecret));
+            byte[] signKey = Encoding.UTF8.GetBytes(UpperCaseUrlEncode(consumerSecret) + "&" + (isRequestToken ? "" : UpperCaseUrlEncode(requestTokenSecret)));
             byte[] urlToSign = Encoding.UTF8.GetBytes("GET&" + UpperCaseUrlEncode(baseUrl) + "&" + UpperCaseUrlEncode(signedParams));
             byte[] hash = new HMACSHA1(signKey).ComputeHash(urlToSign);
             string signature = UpperCaseUrlEncode(Convert.ToBase64String(hash, 0, hash.Length));
@@ -141,7 +80,7 @@ namespace DropboxWP
                 + ", oauth_nonce=\"" + nonce + "\""
                 + ", oauth_signature_method=\"HMAC-SHA1\""
                 + ", oauth_timestamp=\"" + timestamp + "\""
-                + ", oauth_token=\"" + requestToken + "\""
+                + (isRequestToken ? "" : ", oauth_token=\"" + requestToken + "\"")
                 + ", oauth_signature=\"" + signature + "\"";
 
             Debug.WriteLine("INPUT: " + Encoding.UTF8.GetString(urlToSign, 0, urlToSign.Length));
@@ -173,17 +112,29 @@ namespace DropboxWP
                     }
                     if (token.Length > 0 && secret.Length > 0)
                     {
-                        // Step 3 completed, token and secret where authorized
-                        Debug.WriteLine("Authorized: Token: " + token + ", Secret: " + secret);
-                        try
+                        if (isRequestToken)
                         {
-                            accessToken = token;
-                            accessTokenSecret = secret;
-                            NavigationService.Navigate(new Uri("/MainPage.xaml?token=" + accessToken + "&secret=" + accessTokenSecret, UriKind.Relative));
+                            // Step 1 complete, got unauthorized token and secret
+                            //  -> Step 2, launch web browser
+                            requestToken = token;
+                            requestTokenSecret = secret;
+                            Debug.WriteLine("Unauthorized: Token: " + requestToken + ", Secret: " + requestTokenSecret);
+                            Dispatcher.BeginInvoke(new Action(() => webBrowser1.Source = new Uri("https://www.dropbox.com/1/oauth/authorize?oauth_token=" + token + "&oauth_callback=http://localhost")));
                         }
-                        catch (Exception ex)
+                        else
                         {
-                            Debug.WriteLine(ex.Message);
+                            // Step 3 completed, token and secret where authorized
+                            Debug.WriteLine("Authorized: Token: " + token + ", Secret: " + secret);
+                            try
+                            {
+                                accessToken = token;
+                                accessTokenSecret = secret;
+                                NavigationService.Navigate(new Uri("/MainPage.xaml?token=" + accessToken + "&secret=" + accessTokenSecret, UriKind.Relative));
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.WriteLine(ex.Message);
+                            }
                         }
                     }
                 }
@@ -194,7 +145,7 @@ namespace DropboxWP
             }, request);
         }
 
-        public long GetEpochTime()
+        private long GetEpochTime()
         {
             DateTime dtCurTime = DateTime.Now;
             DateTime dtEpochStartTime = Convert.ToDateTime("1/1/1970 8:00:00 AM");
@@ -205,7 +156,7 @@ namespace DropboxWP
             return epochtime;
         }
 
-        public static string UpperCaseUrlEncode(string s)
+        private static string UpperCaseUrlEncode(string s)
         {
             char[] temp = HttpUtility.UrlEncode(s).ToCharArray();
             for (int i = 0; i < temp.Length - 2; i++)
